8b9a345c04f24eb833a78f02db16ddf81448bc2b
Adria <adria.jorda.m@gmail.com>
2025-06-27 10:18:23 +0200

    [add] Leadtech obtenido las palabras

--- enterprise_scrappers/Leadtech.py
+++ enterprise_scrappers/Leadtech.py
@@ -8,8 +8,13 @@ def scrape_job_listings(empresa, url):
         page.goto(url)
         page.wait_for_load_state()
         # Ajusta los selectores según la página
-        if page.get_by_text("Backend"):
-            page.get_by_text("Backend").click()
-
+        if page.get_by_text("IT"):
+            h3_elements = page.locator(
+                '//h2[contains(@class, "whr-group") and contains(text(), "IT")]/following-sibling::ul[contains(@class, "whr-items")]//h3[contains(@class, "whr-title")]/a')
+            #h3_elements = page.query_selector_all('h2:-text("IT") + ul > li > h3 > a')
+            #page.get_by_text("Backend").click()
+            for h3 in h3_elements.element_handles():
+                text = h3.inner_text()
+                print(f'Texto de h3: {text}')
 
         browser.close()
